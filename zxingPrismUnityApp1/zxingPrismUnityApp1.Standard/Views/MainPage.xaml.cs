284ae1859e0d52163f344dc5d99eeb4ce923d512
Allister <redtahr@users.noreply.github.com>
2017-07-28 10:40:26 +1200

    couldn't duplicate crash, drat :-(

--- zxingPrismUnityApp1/zxingPrismUnityApp1.Standard/Views/MainPage.xaml.cs
+++ zxingPrismUnityApp1/zxingPrismUnityApp1.Standard/Views/MainPage.xaml.cs
@@ -4,6 +4,10 @@ namespace zxingPrismUnityApp1.Views {
 	public partial class MainPage : ContentPage {
 		public MainPage() {
 			InitializeComponent();
+			var options = new ZXing.Mobile.MobileBarcodeScanningOptions {
+				PossibleFormats = { ZXing.BarcodeFormat.QR_CODE }
+			};
+			zxing.Options = options;
 		}
 
 		private void zxing_OnScanResult(ZXing.Result result) {
